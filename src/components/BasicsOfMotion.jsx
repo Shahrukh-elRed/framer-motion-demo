3db1c62c0dd5776ab6d1d4859df7315438d07cc6
Shahrukh <shahrukh1494@gmail.com>
2024-06-06 18:36:50 +0530

    completed basics of motion

--- src/components/BasicsOfMotion.jsx
+++ src/components/BasicsOfMotion.jsx
@@ -15,19 +15,23 @@ const BasicsOfMotion = () => {
                         <motion.div 
                             initial={{
                                 rotate: "0deg",
-                                scale: 0
+                                scale: 0,
+                                y: 0
                             }}
                             animate={{
                                 rotate: "180deg",
-                                scale: 1
+                                scale: 1,
+                                y: [0, 150, -150, -150, 0]
                             }}
                             exit={{
                                 rotate: "0deg",
-                                scale: 0
+                                scale: 0,
+                                y: 0
                             }}
                             transition={{
                                 duration: 1,
-                                ease: "backInOut"
+                                ease: "backInOut",
+                                times: [0, 0.25, 0.5, 0.85, 1]
                             }}
                             className="basics_of_motion_square">  
                         </motion.div>
