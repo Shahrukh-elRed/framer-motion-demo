b46a0009126010e2d7742f95efefdd1160ed64a7
Shahrukh <shahrukh1494@gmail.com>
2024-06-06 18:30:24 +0530

    rotate on button click with smooth animation

--- src/components/BasicsOfMotion.jsx
+++ src/components/BasicsOfMotion.jsx
@@ -1,26 +1,40 @@
-import { backInOut, motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const BasicsOfMotion = () => {
-  return (
-    <div className="basics_of_motion">
-        <motion.div 
-            initial={{
-                rotate: "0deg"
-            }}
-            animate={{
-                rotate: "180deg"
-            }}
-            exit={{
-                rotate: "0deg"
-            }}
-            transition={{
-                duration: 1,
-                ease: backInOut
-            }}
-            className="basics_of_motion_square">  
-        </motion.div>
-    </div>
-  )
+    const [isVisible, setIsVisible] = useState(true)
+
+    return (
+        <div className="basics_of_motion">
+            <motion.button onClick={() => setIsVisible(!isVisible)} className="example_button" layout>
+                Show / Hide
+            </motion.button>
+            <AnimatePresence mode="popLayout">
+                {
+                    isVisible && 
+                        <motion.div 
+                            initial={{
+                                rotate: "0deg",
+                                scale: 0
+                            }}
+                            animate={{
+                                rotate: "180deg",
+                                scale: 1
+                            }}
+                            exit={{
+                                rotate: "0deg",
+                                scale: 0
+                            }}
+                            transition={{
+                                duration: 1,
+                                ease: "backInOut"
+                            }}
+                            className="basics_of_motion_square">  
+                        </motion.div>
+                }
+            </AnimatePresence>
+        </div>
+    )
 }
 
 export default BasicsOfMotion
