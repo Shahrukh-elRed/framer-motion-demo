7052d374fdd67763dcfb8f2c66563b5fdc6cfe6c
Shahrukh <shahrukh1494@gmail.com>
2024-06-07 16:09:12 +0530

    completed horizontal scroll carousel

--- src/components/HorizontalScrollCarouselPage/HorizontalScrollCarousel.jsx
+++ src/components/HorizontalScrollCarouselPage/HorizontalScrollCarousel.jsx
@@ -1,7 +1,27 @@
+import { useRef } from "react";
+import Card from "./Card"
+import { motion, useScroll, useTransform } from "framer-motion";
+
 const HorizontalScrollCarousel = ({ cardData }) => {
-  return (
-    <div>Horizontal Scroll Carousel</div>
-  )
+    const targetRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+    });
+    const scrollX = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+    return (
+        <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+            <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+                <motion.div style={{ x: scrollX }} className="flex gap-4">
+                    {
+                        cardData.map(card => 
+                            <Card key={card.id} card={card} />
+                        )
+                    }
+                </motion.div>
+            </div>
+        </section>
+    )
 }
 
 export default HorizontalScrollCarousel
